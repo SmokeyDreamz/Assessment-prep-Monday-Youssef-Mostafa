83acbc5831fdc07a1fa609a87a55bb689f2725b3
Youssef Mostafa <youssefm32@nycstudents.net>
2025-10-06 08:39:50 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,12 +1,15 @@
 #10/3/25 Test prep
 
 
-def language(x):
+
+
+# Language determiner
+""" def language(x):
     t_count = 0
     s_count = 0
 
     for _ in range(x):
-        line = input()
+        line = input("WRITE A SENTENCE FOR THIS LINE.")
         t_count += line.lower().count('t')
         s_count += line.lower().count('s')
 
@@ -18,5 +21,25 @@ def language(x):
         print("French")
 
 
-n = int(input())
-language(n)
+n = int(input("WRITE A NUMBER OF LINES."))
+language(n)  """
+
+
+
+
+# Parking space
+
+""" n = int(input("WRITE A NUMBER OF PARKING SPACES."))
+
+# Read parking space states
+yesterday = input("WRITE THE PARKING SPACES FROM YESTERDAY.")
+today = input("WRITE THE PARKING SPACES FROM TODAY.")
+
+# Count spaces occupied both yesterday and today
+count = 0
+for i in range(n):
+    if yesterday[i] == 'C' and today[i] == 'C':
+        count += 1
+
+# Output the result
+print(f"THE NUMBER OF PARKING SPACES OCCUPIED ON BOTH DAYS ARE {count}.") """
