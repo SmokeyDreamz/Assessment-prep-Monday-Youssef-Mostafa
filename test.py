60c0ce02cd3ded473d925cea9e456eebad892c80
Youssef Mostafa <youssefm32@nycstudents.net>
2025-10-09 10:13:17 -0400

    Update test.py

--- test.py
+++ test.py
@@ -59,7 +59,7 @@ occupied space or . to indicate it was an empty parking space.
 Output Specification
 Output the number of parking spaces which were occupied yesterday and today. """
 
-# Occupy Parking
+""" # Occupy Parking
 
 n = int(input("HOW MANY PARKING SPACES ARE THERE IN TOTAL."))
 
@@ -74,4 +74,54 @@ for i in range(n):
         count += 1
 
 # Output the result
-print(f"THE NUMBER OF PARKING SPACES OCCUPIED ON BOTH DAYS ARE {count}.")
+print(f"THE NUMBER OF PARKING SPACES OCCUPIED ON BOTH DAYS ARE {count}.") """
+
+
+
+
+
+
+
+
+""" Magnus lost a game of chess to Kile so he found comfort in competitive programming. Very soon, he heard of the
+iconic COCI competition and decided to try his luck there.
+He wrote a mail to Kile: "Dear Kile, please, prepare me for COCI. Magnus".
+Kile replied: "You want to participate in COCI? All right, here's your warm-up task. A series of four consecutive letters of
+some word that make up the subword HONI (Croatian acronym for COCI) is called the HONI-block. I will send you the
+word of length and you will throw out as many letters as you want (it might be none as well) so that in the end there
+are as many HONI-blocks as possible in the word. Kile".
+Magnus was very worried and asked you, COCI competitive scene, for help. Help him determine the maximum number
+of HONI-blocks he can get in the final word.
+Input
+The first line contains a word of length , consisting of uppercase letters of the English alphabet.
+Output
+In the first and only line, print out the required number of HONI-blocks. """
+
+# Contest 3 #1 Magnus
+
+
+
+#doesnt work
+""" def honifinder(honi):
+    HONI = 0
+    H = 0
+    O = 0
+    N = 0
+    I = 0
+    for i in honi:
+        if honi[i] == "H":
+            H += 1
+            i += 1
+            if honi[i] == "O":
+                O += 1
+                i += 1
+                if honi[i] == "N":
+                    N += 1
+                    i += 1
+                    if honi[i] == "I":
+                        I += 1
+                        i += 1
+                        if honi[i] == "H" and honi[i] == "O" and honi[i] == "N" and honi[i] == "I":
+                            HONI += 1
+    print(HONI)
+honifinder("HJJJOOONNNKKKII") """
